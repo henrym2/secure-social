8e861fc21df15ae22dc7ef2c30e4baee8529c734
Matthew Henry <henrym2@tcd.ie>
2020-03-19 22:25:14 +0000

    Added decrypt

--- src/components/Feed.vue
+++ src/components/Feed.vue
@@ -8,6 +8,8 @@
 <script>
 import NewPost from './NewPost'
 import Post from './Post'
+import chef from 'cyberchef'
+// const { user, post } = require("../js/constructors")
 const BOX_ID = process.env.VUE_APP_BOX_ID
 
 export default {
@@ -47,6 +49,44 @@ export default {
       this.$jsonbox.create(post, BOX_ID, "posts").then((res) => {
         this.posts.append(res)
       })
+    },
+    getPosts(){
+      this.$jsonbox.read(BOX_ID, "posts", {sort: "_createdOn", limit: 10}).then(
+        (result) => {
+          this.posts = result
+          let toDecrypt = []
+          let promises = []
+          //Get a list of posts that can be decrypted and a list of their respective authors as promises
+          this.posts.foreach(p => {
+            if(p.trusted.includes(this.user._id)){
+              toDecrypt.push(p)
+              promises.push(this.$jsonbox.read(BOX_ID, "users", p.author))
+            }
+          Promise.all(promises, (result) => {
+            let aKeys = result.flat().map(a => a.secret)
+            let decrypted = toDecrypt.map((d,index) => {
+              return chef.bake(d.body, [
+                  { "op": "AES Decrypt",
+                    "args": [
+                      { "option": "UTF8", "string": aKeys[index] },
+                      { "option": "UTF8", "string": process.env.VUE_APP_SECRET },
+                      "CBC", "Raw", "Hex"
+                    ] 
+                  }
+              ])
+            })
+            this.posts = this.posts.map(p => {
+              let obj = p
+              let decrypt = decrypted.find(d => d._id == obj._id)
+              if (decrypt != undefined) {
+                obj.body = decrypt.body
+                return obj
+              }
+              return obj
+            })
+          })
+          })
+        })
     }
   }
 }
